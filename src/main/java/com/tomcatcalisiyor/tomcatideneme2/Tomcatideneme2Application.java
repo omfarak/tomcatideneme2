2aede3ecb97f42bd50b50edb193bccc1fda30227
omfarak <116785031+omfarak@users.noreply.github.com>
2024-07-09 14:43:33 +0200

    Initial commit

---
+++ src/main/java/com/tomcatcalisiyor/tomcatideneme2/Tomcatideneme2Application.java
@@ -0,0 +1,13 @@
+package com.tomcatcalisiyor.tomcatideneme2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tomcatideneme2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tomcatideneme2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomcatcalisiyor/tomcatideneme2/Tomcatideneme2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomcatcalisiyor.tomcatideneme2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tomcatideneme2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
